c4ebf66f9227438e3478d4f2128cfaff59e45217
itimoreo <pol.lopezpro@gmail.com>
2024-07-29 00:13:53 +0200

    ajout du choix du password lenght

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const passwordBox = document.getElementById('password');
-const lenght = 12;
-
-const uperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
-const lowerCase = 'abcdefghijklmnopqrstuvwxyz'; 
-const numbers = '0123456789';
-const special = '!@#$%^&*()_+{}:"<>?[];,./`~';
-
-const allChars = uperCase + lowerCase + numbers + special;
-
 function generatePassword() {
+    const length = document.getElementById('length').value;
     let password = '';
     password += uperCase[Math.floor(Math.random() * uperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += numbers[Math.floor(Math.random() * numbers.length)];
     password += special[Math.floor(Math.random() * special.length)];
 
-    while(lenght > password.length){
+    while(password.length < length){
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
     passwordBox.value = password;
@@ -26,3 +17,12 @@ function copyPassword() {
     document.execCommand('copy');
     alert('Password copied to clipboard');
 }
+
+const passwordBox = document.getElementById('password');
+
+const uperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'; 
+const numbers = '0123456789';
+const special = '!@#$%^&*()_+{}:"<>?[];,./`~';
+
+const allChars = uperCase + lowerCase + numbers + special;
